9c8940d07f5d4e29f0b26d4dc26429560ccffe55
JiayingShentu <2386721963@qq.com>
2023-04-17 15:45:21 +0800

    中期

--- 学长代码测试/preprocess1.py
+++ 学长代码测试/preprocess1.py
@@ -6,39 +6,40 @@ from typing import List, Dict
 import json
 from tqdm import tqdm
 
-def get_file_paths(dir) -> List[str]:
+def label_num_transform(label_list: List) -> Dict:
     """
-    获得目录下所有文件路径的列表
-    :param dir: 目录路径
-    :return: 包含文件路径的列表
+    将标签和数字相互转换
+    :param label_list: 标签列表
+    :return: {标签：数字}形式的字典 和 {数字：标签}形式的字典
     """
-    file_paths = []
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            file_paths.append(os.path.join(root, file))
-    return file_paths
+    labels = set(label_list)
+    label_dict = dict(zip(labels, range(len(labels))))
+    num_dict = {}
+    for key, value in label_dict.items():
+        num_dict[value] = key
+    return label_dict, num_dict
+
+def write_to_json(path, mydict):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(mydict, f, ensure_ascii=False, indent=4)
+
+def read_from_json(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+        
 
 if __name__ == '__main__':
-    files = get_file_paths('音频')
-    print(files)
+    files = 'D:\\毕设相关\\苏州训练\\音频\\'
 
-    # # 获得标签文本和数字的对应
-    # label_dict_path = 'label_dict.json'
-    # num_dict_path = 'num_dict.json'
-    # if not os.path.exists(label_dict_path) or not os.path.exists(num_dict_path):
-    #     label_list = []
-    #     for file in files:
-    #         label = file.split(os.sep)[-2]  # 标签的获取根据目录结构更改
-    #         label_list.append(label)
-    #     label_dict, num_dict = label_num_transform(label_list)
-    #     write_to_json(label_dict_path, label_dict)
-    #     write_to_json(num_dict_path, num_dict)
-
-    # else:
-    #     label_dict = read_from_json(label_dict_path)
-    #     num_dict = read_from_json(num_dict_path)
-
-    # # print(label_dict, num_dict)
+    # 获得标签文本和数字的对应
+    label_list = []
+    for file in os.listdir(files):
+        label = file  # 标签的获取根据目录结构更改
+        print(label)
+        label_list.append(label)
+    label_dict, num_dict = label_num_transform(label_list)
+    print(label_dict,num_dict)
 
     # # 把音频转成mfcc形式的文件, 并将mfcc和标签保存为.npy文件
     # mfcc_path = 'mfccs.npy'
